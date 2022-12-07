8fc26590e6b7a7e523365c1b5d5f4eeb7e59ffc9
mirandalyng <lyngmiranda@gmail.com>
2022-12-07 13:49:52 +0100

    changed the postUser added highest id etc.
    Also added the database of user id to the right one.

--- PHP(Back-End)/postUser.php
+++ PHP(Back-End)/postUser.php
@@ -18,48 +18,48 @@ if(!isset($r_data["username"], $r_data["password"], $r_data["age"])){
 }
 
 //Checks that the user data isn't empty and that the user is over 18 and the lenght of so the password is shorter than 5 characters 
-if($r_data["username"] == "" 
-or $r_data["password"] == ""
-or $r_data["age"] < 18){
+if($r_data["username"] == "" or $r_data["password"] == ""or $r_data["age"] < 18){
 
     $error = ["error" => "Missing data."];
     sendJSON($error, 404);
 }
 
-//Checks that the user isn't already in the database of users 
-// if($r_data["username"] == $userDatabase["username"]){
-//     $error = ["error" => "Username already taken. Please try again."];
-//     sendJSON($error, 400);
-// }
-
-    
-
-    $username = $r_data["username"];
-    $password = $r_data["password"];
-    $age = $r_data["age"];
-
-    
-    //The highest ID starts at 0 
-    $highest_id = 0; 
-
-    //Controls the highest ID in the array of users 
-    foreach($users as $user){
-        if($user["userId"] > $highest_id){
-            $highest_id = $user["userId"];
-        }
+//Checks that the user isn't already in the database of users
+foreach($users as $user){
+    if($user["username"] == $r_data["username"] ){
+        $error = ["error" => "The username is already taken. Try a new one."];
+        sendJSON($error, 400);
     }
+}
 
-    //The new id of the user is the highest existing ID + 1 
-    $new_id = $highest_id + 1; 
+//variables for the data 
 
-    //The new user contains the id, username, age and the empty array of likedBeers 
-    $newUser = ["userId" => $new_id, "username" => $username, "password" => $password ,"age" => $age, "likedBeers" => []];
+$username = $r_data["username"];
+$password = $r_data["password"];
+$age = $r_data["age"];
+
     
-    //Adding the user to the array of users and updates the database 
-    $users[] = $newUser;
-    $users_json = json_decode($users, JSON_PRETTY_PRINT); 
-    file_put_contents($userDatabase, $users_json);
-    sendJSON($newUser);
+//The highest ID starts at 0 
+$highest_id = 0; 
+
+//Controls the highest ID in the array of users 
+foreach($users as $user){
+    if($user['userId'] > $highest_id){
+        $highest_id = $user["userId"];
+    }
+}
+
+//The new id of the user is the highest existing ID + 1 
+$new_id = $highest_id + 1; 
+
+//The new user contains the id, username, age and the empty array of likedBeers 
+$newUser = ["userId" => $new_id, "username" => $username, "password" => $password ,"age" => $age, "likedBeers" => []];
+    
+//Adding the user to the array of users and updates the database 
+$users[] = $newUser;
+$users_json = json_encode($users, JSON_PRETTY_PRINT); 
+file_put_contents($userDatabase, $users_json);
+sendJSON($newUser);
 
 
 
