c4ef37cfd47046d1c6d13210d9ba1b566dbb220b
My Bergquist Rajic <mybergqvist@gmail.com>
2022-12-07 11:12:07 +0100

    beginning

---
+++ PHP(Back-End)/read_beerDatabase.php
@@ -0,0 +1,24 @@
+<?php
+    require_once ("functions.php");
+
+    // If request method is not GET
+    if($request_method !== "GET"){
+        $error = ["error" => "The method must be GET"]; 
+        sendJSON($error, 400);
+        exit();
+    }
+    // If username, password or beers is not set in get paramaters
+    if(!isset($_GET["un"], $_GET["pw"], $_GET["beers"])){
+        $error = ["error" => "The parameters: username, password or beers are not set"]; 
+        sendJSON($error, 400);
+        exit();
+    }
+
+    $sucess = ["succes" => "u gots"]; 
+    sendJSON($sucess, 400);
+    exit();
+
+
+
+
+?>
