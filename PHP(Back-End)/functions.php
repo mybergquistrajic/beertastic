24d138a0e1c5a5670ee4473105421bd111663431
mirandalyng <lyngmiranda@gmail.com>
2022-12-07 10:20:45 +0100

    Update functions.php

--- PHP(Back-End)/functions.php
+++ PHP(Back-End)/functions.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 //Variables 
 
 //JSON - files 
@@ -20,8 +18,10 @@ $r_json = file_get_contents("php://input");
 //Requested data 
 $r_data = json_decode($r_json, true);
 
-//IF-STATMENTS
+//Array of users 
+$users = [];
 
+//IF-STATMENTS
 //If statment to check that the content-type is JSON if the method is not GET
 if($request_method != "GET"){
     if($content_type != "application/json"){
@@ -30,14 +30,17 @@ if($request_method != "GET"){
     }
 }
 
-//Array of users 
-$users = [];
-
 //if the userDatabase.json exists uptade and decode it
 if(file_exists($userDatabase)){
     $json = file_get_contents($userDatabase);
     $users = json_decode($json, true);
 }
 
-
+//Funtion for sending JSON-data 
+function sendJSON($data, $statusCode = 200) {
+    header('Content-Type: application/json');
+    http_response_code($statusCode);
+    echo json_encode($data);
+    exit();
+}
 ?>
