7484e49ca09905d3e2e598d90a8f979e20554134
My Bergquist Rajic <mybergqvist@gmail.com>
2022-12-07 14:52:53 +0100

    fix

--- PHP(Back-End)/postReview.php
+++ PHP(Back-End)/postReview.php
@@ -16,7 +16,7 @@
     }
 
     // Finding the right beer
-    foreach($beers as $beer){
+    foreach($beers as $index => $beer){
         if($beer["id"] == $r_data["beerId"]){
             $highestId = 0;
             // For each review, change highestId
@@ -27,13 +27,20 @@
             }
             $highestId = $highestId + 1;
             
-            // New review
-            $newReview = ["review_id" => $highestId, "username" => $r_data["username"], "rating" => $r_data["rating"], "message" => $r_data["reviewContent"]];
+            // Create and add new review to array
+            $newReview = ["review_id" => $highestId, "username" => $r_data["username"], "rating" => $r_data["rating"], "message" => $r_data["reviewContent"], "date" => date("y-m-d")];
+            array_push($beer["reviews"], $newReview);
 
-            // ADD:
-                // Date to review
-                // Update the beer review list with new review (encode, put)
-                // Send JSON
+            // Remove old beer, add new beer (including new review), and sort
+            $updatedBeer = $beer;
+            array_splice($beers, $index, 1);
+            $beers[] = $updatedBeer;
+            array_multisort($beers);
+
+            // Update database and send JSON
+            $beersJSON = json_encode($beers, JSON_PRETTY_PRINT);
+            $beersData = file_put_contents($beerDatabase, $beersJSON);
+            sendJSON($newReview);
         }
     }
 
