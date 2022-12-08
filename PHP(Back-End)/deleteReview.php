2583bd26c7214f5e209296963d140ad3a7672441
mirandalyng <lyngmiranda@gmail.com>
2022-12-08 14:00:50 +0100

    Added comments.

--- PHP(Back-End)/deleteReview.php
+++ PHP(Back-End)/deleteReview.php
@@ -5,7 +5,7 @@ require_once "functions.php";
 
 ini_set("display_errors", 1);
 
-//Checks that the method used is DELETE 
+//Checks that the method is DELETE 
 if ($request_method != "DELETE"){
     $error = ["error" => "Wrong method used."];
     sendJSON($error, 400);
@@ -13,24 +13,29 @@ if ($request_method != "DELETE"){
 
 
 
-//Checks that the requestet data contains "id" and 
+//Checks that the requested data contains "id" 
+
 if(isset($r_data["review_id"])){
 
+    //Loops through the beers in the database
     foreach($beers as $beer){
-        
+        //Loops through the array of reviews 
         foreach($beer["reviews"] as $index => $review){
+            //If the review id is the same as the requested data id delete delete it
             if($review["review_id"] == $r_data["review_id"]){
                 
                 array_splice($review, $index, 1);
 
+                //Adding the changes to the beerDatabase 
                 $beers_json = json_encode($beers, JSON_PRETTY_PRINT);
                 file_put_contents($beerDatabase, $beers_json);
                 sendJSON($review["review_id"]);  
             }
         }
-    }
-}else{
-    $error = ["error" => "Bad request. The object doesn't exist."];
-    sendJSON($error, 404);
-}
+    }       
+            //If the request doen't include id send error message 
+            }else{
+                $error = ["error" => "Bad request. The object doesn't exist."];
+                sendJSON($error, 404);
+            }
 ?>
