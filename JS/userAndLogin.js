4ba88210345071ec1078e51d8cf880f55b84db3a
mirandalyng <lyngmiranda@gmail.com>
2022-12-09 13:58:02 +0100

    user and login updates
    I have begun on the funcitons for the log in answer , the log in and the favorites for the user.

--- JS/userAndLogin.js
+++ JS/userAndLogin.js
@@ -1,17 +1,35 @@
-// function favorites (user){
-//     user.favorites.forEach favorite (renderBeers(favorite))
-//     }
+"use strict";
 
-//     function showLogInView(){}
+//USER LOGIN
+//function to log in with the user_status to define logged in or not
+function logIn(user_status) {
+  //request to log in PHP-file
+  const user_status_request = new Request("login.php");
 
-//     function logIn (user_status) {
-//     fetch-request till users.json
-//     the right URL
-//     response from the promise
-//     recourse  that calls the function logIn_answers (user_resource (0, 1) )
+  //fetch the request , when the resource promise comes call the function logIn_answer
+  fetch(user_status_request)
+    .then((r) => r.json())
+    .then(logIn_answer(user_status));
+}
 
-//     function logIn_answer ()
-//     if answer ==  200
-//     showLogInView ( )
+//Function for the response to the request to log in
+function logIn_answer(response_answer) {
+  //If the user is in the database and the log in is sucessful show the log in view.
+  if (response_answer === 200) {
+    showLogInView();
+  } else {
+    //error message here?
+  }
+}
 
-//     }
+//function for the view while logged in
+function showLogInView() {}
+
+//FAVORITES LIST (The hearts)
+//Loops through the user and for each favorite render beerBeers favorite.
+function favorites(user) {
+  //Loops through the users and rendersBeers based on the users favorites
+  user.favorites.forEach((favorite) => {
+    renderFavorites(favorite);
+  });
+}
