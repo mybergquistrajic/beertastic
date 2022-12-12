7ec2d0b69c6be0224cd7fd3e875fb428d009cbe1
mirandalyng <lyngmiranda@gmail.com>
2022-12-12 14:33:03 +0100

    Changed and commented on the login user
    Added the body and method to the request and commented on the code Also added the render Popup for no user found in the database.

--- JS/userAndLogin.js
+++ JS/userAndLogin.js
@@ -1,12 +1,28 @@
 "use strict";
-
+//Global variabel for user to see if logged in or not.
 let globalUser;
 
 //USER LOGIN
 //function to log in with the user_status to define logged in or not
 function logIn(user_status) {
+  const username = document.getElementById("logIn_username").value;
+  const password = document.getElementById("logIn_password").value;
+
+  //user that is in the body with username and password
+  const user = {
+    username: username,
+    password: password,
+  };
+
+  //the method, body and headers in the request
+  const options = {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: { "Content-type": "application/json" },
+  };
+
   //request to log in PHP-file
-  const user_status_request = new Request("login.php");
+  const user_status_request = new Request("login.php", options);
   //fetch the request , when the resource promise comes call the function logIn_answer
   fetch(user_status_request)
     .then((r) => r.json())
@@ -17,12 +33,15 @@ function logIn(user_status) {
 function logIn_answer(response, username) {
   //If the user is in the database and the log in is sucessful show the log in view.
   if (response === 200) {
+    //Update the global variabel to the username
     globalUser = username;
+    //Run the funciton for the Log in view
     showLogInView();
   } else {
-    globalUser = 0
-    // render popup
-    //error message here?
+    //Update the global variabel to 0 to show that user is not logged in
+    globalUser = 0;
+    // render popup for no user found in the databas
+    renderPopUp("NoUserFound");
   }
 }
 
