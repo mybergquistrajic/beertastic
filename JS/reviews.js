477a100b0d183dd571e5c309597987db13aac5e8
MadeByGabbe <97948127+MadeByGabbe@users.noreply.github.com>
2022-12-12 16:41:47 +0100

    Updated renderReviews with support for writeReview
    Includes a little bit of cleanup

--- JS/reviews.js
+++ JS/reviews.js
@@ -7,72 +7,102 @@ function renderReviews(beer) {
 
     // render a review for each review in the array
     reviews.forEach(function (review) {
-        const reviewContainer = document.createElement("div");
-        reviewContainer.classList.add("review");
 
-        // if user has written review, add delete button
-        if (review.username === user) {
-            const deleteButton = document.createElement("div");
-            deleteButton.classList.add("reviewDelete");
-            deleteButton.innerHTML = //trashcan icon?
-            deleteButton.addEventListener("click", function () {
+        // if reviewMessage is not empty, render review
+        if (review.message != "") {
+            const reviewContainer = document.createElement("div");
+            reviewContainer.classList.add("review");
+
+            // if user has written review, add delete button
+            if (review.username === user) {
+                const deleteButton = document.createElement("div");
+                deleteButton.classList.add("reviewDelete");
+                deleteButton.innerHTML = //trashcan icon?
+                    deleteButton.addEventListener("click", function () {
                 
-                // Confirm delete review
-                renderPopup("deleteReview");
+                        // Confirm delete review
+                        renderPopup("deleteReview");
 
-                const yesButton = document.querySelector(".yesButton");
-                yesButton.addEventListener("click", function () {
-                    fetch("http://localhost:8888/deleteReview",
-                    {
-                        method: "DELETE",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            beerId: beer.id,
-                            reviewId: review.id
-                        })
+                        const yesButton = document.querySelector(".yesButton");
+                        yesButton.addEventListener("click", function () {
+                            fetch("http://localhost:8888/deleteReview",
+                                {
+                                    method: "DELETE",
+                                    headers: {
+                                        "Content-Type": "application/json"
+                                    },
+                                    body: JSON.stringify({
+                                        beerId: beer.id,
+                                        reviewId: review.id
+                                    })
                         
-                    })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data)
-                        renderReviews(beer)
-                    })
-                })
+                                })
+                                .then(res => res.json())
+                                .then(data => {
+                                    console.log(data)
+                                    renderReviews(beer)
+                                })
+                        })
 
-                const noButton = document.querySelector(".noButton");
-                noButton.addEventListener("click", function () {
-                    renderPopup("none"); //right way to close popup?
-                })
+                        const noButton = document.querySelector(".noButton");
+                        noButton.addEventListener("click", function () {
+                            renderPopup("none"); //right way to close popup?
+                        })
             
-                reviewContainer.appendChild(deleteButton);
+                        reviewContainer.appendChild(deleteButton);
+                    })
+            }
+
+            // Review username
+            const reviewUser = document.createElement("p");
+            reviewUser.classList.add("reviewUser");
+            reviewUser.innerHTML = review.username;
+            reviewContainer.appendChild(reviewUser);
+
+            // Review date
+            const reviewDate = document.createElement("p");
+            reviewDate.classList.add("reviewDate");
+            reviewDate.innerHTML = review.date;
+            reviewContainer.appendChild(reviewDate);
+
+            // Review message
+            const reviewMessage = document.createElement("p");
+            reviewMessage.classList.add("reviewMessage");
+            reviewMessage.innerHTML = review.message;
+            reviewContainer.appendChild(reviewMessage);
+
+            // if review is longer than 100 characters, add show more button
+            if (review.message.length > 100) {
+                const showMore = document.createElement("p");
+                showMore.classList.add("showMore");
+                showMore.innerHTML = "Show more";
+                // Toggle show more button
+                showMore.addEventListener("click", function () {
+                    reviewMessage.classList.toggle("showMore");
+                    showMore.classList.toggle("showMore");
+                })
+                reviewContainer.appendChild(showMore);
+            }
+
+            // Review rating
+            const reviewRating = document.createElement("p");
+            reviewRating.classList.add("reviewRating");
+            reviewRating.innerHTML = reviewRating(review.rating);
+                reviewContainer.appendChild(reviewRating);
+        
+        
+            const writeReview = document.createElement("div");
+            writeReview.classList.add("writeReview");
+            writeReview.innerHTML = "Write review";
+            writeReview.addEventListener("click", function () {
+                writeReview(beer);
             })
+        
+            reviewsContainer.appendChild(reviewContainer);
         }
+})
+}
 
-        const reviewUser = document.createElement("p");
-        reviewUser.classList.add("reviewUser");
-        reviewUser.innerHTML = review.username;
-        reviewContainer.appendChild(reviewUser);
-
-        const reviewDate = document.createElement("p");
-        reviewDate.classList.add("reviewDate");
-        reviewDate.innerHTML = review.date;
-        reviewContainer.appendChild(reviewDate);
-
-        const reviewMessage = document.createElement("p");
-        reviewMessage.classList.add("reviewMessage");
-        reviewMessage.innerHTML = review.message;
-        reviewContainer.appendChild(reviewMessage);
-
-        // if review is longer than 100 characters, add show more button
-        if (review.message.length > 100) {
-            const showMore = document.createElement("p");
-            showMore.classList.add("showMore");
-            showMore.innerHTML = "Show more";
-            showMore.addEventListener("click", function () {
-                reviewMessage.classList.toggle("showMore");
-                showMore.classList.toggle("showMore");
             })
             reviewContainer.appendChild(showMore);
         }
