aa81d1e6d33727ccc53f2689ad56fc8bfb123985
mirandalyng <lyngmiranda@gmail.com>
2022-12-12 14:38:51 +0100

    Added comments on the popUpDiv

--- JS/popUp.js
+++ JS/popUp.js
@@ -1,4 +1,3 @@
-
 // function renderPopUp (type){
 //     render popup
 //     SWITCH()
@@ -29,9 +28,12 @@
 //Function to load different answers based on the type - parameter.
 
 function renderPopUp(type) {
+  //Creates the popUpDiv
   const popUpDiv = document.createElement("div");
+  //Adds a class for styling the popUpDiv
   popUpDiv.classList.add("popUpDiv");
 
+  //Depending on the type of popUp, the HTML is different.
   if (type === "checkage") {
     popUpDiv.innerHTML = `
       <div>
@@ -69,8 +71,14 @@ function renderPopUp(type) {
     <p>You have to be logged in to view this.</p>
     <button>Log in</button>
     </div>`;
+  } else if (type === "NoUserFound") {
+    popUpDiv.innerHTML = `
+    <div>
+    <h1>Sorry!</h1>
+    <p>No user found. Please create an account.</p>
+    <button>Ok</button>
+    </div>`;
   }
 
   //append the popUpDiv here:
 }
-
